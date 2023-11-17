f05cbc8f43e5b2a68cdaffa2345c68607351b404
Grzesiek333 <grzesiek333@gmail.com>
2023-11-17 20:13:55 +0100

    Navigation_and_main_section-1b

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./components/home";
-
+import Login from "./login";
+import Register from "./register";
+import Logout from "./logout";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />        
+        <Route path="/" element={<Home />} />    
+        <Route path="/logowanie" element={<Login />} />       
+        <Route path="/rejestracja" element={<Register />} />   
+        <Route path="/wylogowano" element={<Logout />} />   
 
       </Routes>
     </Router>
--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -1,10 +1,33 @@
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomeHeader() {
     return (
       <>
-        <h1>Home Header</h1>
+      <section className="HomeHeader">
+        <div style={{backgroundImage: "url('src/assets/Home-Hero-Image.jpg')", backgroundPosition: 'top right', backgroundRepeat: 'no-repeat'}}>          
+        </div>
+        <div>
+          <nav className="menu-top">
+            <ul>
+              <li><Link to="/logowanie">Zaloguj</Link></li>
+              <li><Link to="/rejestracja">Załóż konto</Link></li>
+            </ul>
+          </nav>
+          <nav className="menu">
+            <ul>
+              <li><Link to="">Start</Link></li>
+              <li><Link to="">O co chodzi?</Link></li>
+              <li><Link to="">O nas</Link></li>
+              <li><Link to="">Fundacja i oranizacje</Link></li>
+              <li><Link to="">Kontakt</Link></li>
+            </ul>
+          </nav>
+          <h1>Zacznij pomagać!<br /> Oddaj niechciane rzeczy w zaufane ręce</h1>
+          <img src="src/assets/Decoration.svg"/>
+          
+        </div>
+      </section>
       </>
     );
   }
