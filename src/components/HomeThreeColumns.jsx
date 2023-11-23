117224b1f16213cbe7f7674088912f9ad59e9185
Grzesiek333 <grzesiek333@gmail.com>
2023-11-23 21:55:09 +0100

    improved sass

--- src/components/HomeThreeColumns.jsx
+++ src/components/HomeThreeColumns.jsx
@@ -2,17 +2,17 @@ export default function HomeThreeColumns() {
     return (
       <>
         <section className="ThreeColumns">
-          <div>
+          <div className="boxs_ThreeColums">
             <div className="Number_ThreeColumns">10</div>
             <div className="Title_ThreeColumns">ODDANYCH WORKÓW</div>
             <div className="Text_ThreeColumns">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
           </div>
-          <div>
+          <div className="boxs_ThreeColums">
             <div className="Number_ThreeColumns">5</div>
             <div className="Title_ThreeColumns">WSPARTYCH ORGANIZACJI</div>
             <div className="Text_ThreeColumns">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
           </div>
-          <div>
+          <div className="boxs_ThreeColums">
             <div className="Number_ThreeColumns">7</div>
             <div className="Title_ThreeColumns">ZORGANIZOWANYCH ZBIÓREK</div>
             <div className="Text_ThreeColumns">Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</div>
